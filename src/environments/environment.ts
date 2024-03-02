e94296ad64a8541338665e93b505eaa1bc5ff7df
ahmedbeheiry <ahmed_beheiry@yahoo.com>
2024-03-02 15:04:33 +0200

    edit api base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiBaseURL: 'https://5f009ed807605200169e6a86.mockapi.io/api/v1/',
+	apiBaseURL: 'https://5f009ed807605200169e6a86.mockapi.io/api/v1',
 };
